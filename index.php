0375a418f489ec765b286d330e2cac674a197241
Kamil W <kamiolek01@gmail.com>
2024-02-02 13:26:47 +0100

    php działa, dodany przycisk z js, napraw double clicka na buttonie, zmienić rozwijany napis w buttonie

--- index.php
+++ index.php
@@ -21,22 +21,22 @@ $use4 = mysqli_query($conn, $kw4);
 $kw5 = "SELECT Nazwa FROM zadania WHERE dzien_tygodnia = 5";
 $use5 = mysqli_query($conn, $kw5);
 
-echo "<div class='glowny'>
-<div id='poniedzialek' class='dzien'><h2>Poniedziałek</h2><ul><li>";
+echo "<div class='main'>
+<div id='poniedzialek' class='day'><h2>Poniedziałek</h2><ul><li>";
 while($tab=mysqli_fetch_row($use1)){echo "$tab[0]";}
-echo"</li></ul></div>
-<div id='wtorek' class='dzien'><h2>Wtorek</h2><ul><li>";
+echo"</li></ul><button class='show' onclick='fshow1()''>Show</button><p id='pon'>If there is no element with the given id, this function returns null. Note that the id parameter is</p></div>
+<div id='wtorek' class='day'><h2>Wtorek</h2><ul><li>";
 while($tab=mysqli_fetch_row($use2)){echo "$tab[0]";}
-echo "</li></ul></div>
-<div id='sroda' class='dzien'><h2>Środa</h2><ul><li>";
+echo "</li></ul><button class='show' onclick='fshow2()''>Show</button><p id='wt'>If there is no element with the given id, this function returns null. Note that the id parameter is</p></div>
+<div id='sroda' class='day'><h2>Środa</h2><ul><li>";
 while($tab=mysqli_fetch_row($use3)){echo "$tab[0]";}
-echo "</li></ul></div>
-<div id='czwartek' class='dzien'><h2>Czwartek</h2><ul><li>";
+echo "</li></ul><button class='show' onclick='fshow3()''>Show</button><p id='sr'>If there is no element with the given id, this function returns null. Note that the id parameter is</p></div>
+<div id='czwartek' class='day'><h2>Czwartek</h2><ul><li>";
 while($tab=mysqli_fetch_row($use4)){echo "$tab[0]";}
-echo "</li></ul></div>
-<div id='piatek' class='dzien'><h2>Piątek</h2><ul><li>";
+echo "</li></ul><button class='show' onclick='fshow4()''>Show</button><p id='czw'>If there is no element with the given id, this function returns null. Note that the id parameter is</p></div>
+<div id='piatek' class='day'><h2>Piątek</h2><ul><li>";
 while($tab=mysqli_fetch_row($use5)){echo "$tab[0]";}
-echo "</li></ul></div>
+echo "</li></ul><button class='show' onclick='fshow5()''>Show</button><p id='pt'>If there is no element with the given id, this function returns null. Note that the id parameter is</p></div>
 </div>";
 ?>
 
--- script.js
+++ script.js
@@ -6,3 +6,43 @@ function updateTime() {
  }
  
  setInterval(updateTime, 1000);
+
+ function fshow2() {
+    var wt = document.getElementById("wt");
+    if (wt.style.display === "none") {
+        wt.style.display = "flex";
+    } else {
+        wt.style.display = "none";
+    }
+  }
+  function fshow1() {
+    var pon = document.getElementById("pon");
+    if (pon.style.display === "none") {
+        pon.style.display = "flex";
+    } else {
+        pon.style.display = "none";
+    }
+  }
+  function fshow3() {
+    var sr = document.getElementById("sr");
+    if (sr.style.display === "none") {
+        sr.style.display = "flex";
+    } else {
+        sr.style.display = "none";
+    }
+  }
+  function fshow4() {
+    if (czw.style.display === "none") {
+        czw.style.display = "flex";
+    } else {
+        czw.style.display = "none";
+    }
+  }
+  function fshow5() {
+    var pt = document.getElementById("pt");
+    if (pt.style.display === "none") {
+        pt.style.display = "flex";
+    } else {
+        pt.style.display = "none";
+    }
+  }
