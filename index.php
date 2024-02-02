9ee6dd346eb42ae7159b9b84302ce9fb8fb57522
Kamil W <kamiolek01@gmail.com>
2024-02-02 12:02:40 +0100

    prawie koniec ale php nie wypisuje zrobionych zadań

--- index.php
+++ index.php
@@ -4,18 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Tygoniowy Harmonogram Pracy</title>
+    <title>Tygodniowy Harmonogram Pracy</title>
 </head>
 <body>
-    <div class="baner"><h1>Tygoniowy Harmonogram Pracy</h1></div>
+    <div class="baner"><h1>Tygodniowy Harmonogram Pracy</h1></div>
+<?php
+$conn = new mysqli("localhost","root","","baza_dziennik");
+$kw1 = "SELECT Nazwa FROM zadania WHERE dzien_tygodnia = 1";
+$use = mysqli_query($conn, $kw1);
+$row = mysqli_fetch_array($use);
 
-    <div class="glowny">
-        <div id="poniedzialek" class="dzien"><h2>Poniedziałek</h2></div>
-        <div id="wtorek" class="dzien"><h2>Wtorek</h2></div>
-        <div id="sroda" class="dzien"><h2>Środa</h2></div>
-        <div id="czwartek" class="dzien"><h2>Czwartek</h2></div>
-        <div id="piatek" class="dzien"><h2>Piątek</h2></div>
-    </div>
+
+?>
+<div class='glowny'>
+<div id='poniedzialek' class='dzien'><h2>Poniedziałek</h2><ul><li>
+<?php
+while ($row = mysqli_fetch_row($use)) {
+    echo $row['Nazwa'];
+}
+?>
+</li></ul></div>
+<div id='wtorek' class='dzien'><h2>Wtorek</h2><ul><li></li></ul></div>
+<div id='sroda' class='dzien'><h2>Środa</h2><ul><li></li></ul></div>
+<div id='czwartek' class='dzien'><h2>Czwartek</h2><ul><li></li></ul></div>
+<div id='piatek' class='dzien'><h2>Piątek</h2><ul><li></li></ul></div>
+</div>
+
+
 
     <footer>
         <a>Wykonał:Kamil</a>
